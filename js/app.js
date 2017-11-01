bad1ffd4f2d4b21557de905eb5ea857e77b97e47
pkewly <paul.kissin@candyusa.com>
2017-11-01 08:01:05 -0400

    1st round

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-/*
-  Please add all Javascript code to this file.
-*/
+
+// Holler at reddit, "Yo, I'm trying to feature your content. Let me get that hook up."
+$.get("https://accesscontrolalloworiginall.herokuapp.com/https://www.reddit.com/r/todayilearned.json", function(results){
+  console.log(results);
+  
+
+
+// Iterating through the contents of the arrays in the api response
+for (i = 0; i < 5; i++) {
+var ignition = results.data.children[i].data;
+
+
+// Printing the contents of the end point to the dom
+   $("#main").append('<article class="article"><section class="featuredImage"><img src="' + ignition.thumbnail + '"/></section>'
+	              +  '<article class="article"><section class="articleContent"><a href="' + ignition.url + '"><h3>' + ignition.title + '</h3></a></section>'
+                  +  '<article class="article"><section class="impressions">' + ignition.score + '</section></article>'
+                  +  '<div class="clearfix"></div></article>');
+}
+
+
+
+
+
+
+
+
+
+ })
+ 		
